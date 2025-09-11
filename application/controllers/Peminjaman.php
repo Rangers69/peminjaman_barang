549c0c40e389d240992c62576f66c45898adea2d
unknown <mf1345710@gmail.com>
2025-09-11 15:05:02 +0700

    peminjaman

--- application/controllers/Peminjaman.php
+++ application/controllers/Peminjaman.php
@@ -169,7 +169,9 @@ class Peminjaman extends CI_Controller {
             }
 
             // Perform the update
-            if ($this->Peminjaman_model->update_peminjaman($id, $data_update)) {
+            if ($this->Peminjaman_model->update_peminjaman($id_peminjaman, $data_update)) {
+                $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Data Berhasil di edit</div>');
+			    redirect('peminjaman');
                 echo json_encode(['status' => 'success', 'message' => 'Data peminjaman berhasil diperbarui!']);
             } else {
                 echo json_encode(['status' => 'error', 'message' => 'Gagal memperbarui data peminjaman.']);

bfc6498e410ef744b7c09b07a0d100317fcf61a3
windi fujiyanti <ezzafaizan74_email@example.com>
2025-10-17 17:10:40 +0700

    perubahan user

--- application/views/user/index.php
+++ application/views/user/index.php
@@ -42,32 +42,6 @@
 			<div class="card-body">
 				<div id="example1_wrapper" class="dataTables_wrapper dt-bootstrap4">
 					<div class="row">
-						<div class="col-sm-12 col-md-6">
-							<div class="dt-buttons btn-group flex-wrap"> <button
-									class="btn btn-secondary buttons-copy buttons-html5" tabindex="0"
-									aria-controls="example1" type="button"><span>Copy</span></button> <button
-									class="btn btn-secondary buttons-csv buttons-html5" tabindex="0"
-									aria-controls="example1" type="button"><span>CSV</span></button> <button
-									class="btn btn-secondary buttons-excel buttons-html5" tabindex="0"
-									aria-controls="example1" type="button"><span>Excel</span></button> <button
-									class="btn btn-secondary buttons-pdf buttons-html5" tabindex="0"
-									aria-controls="example1" type="button"><span>PDF</span></button> <button
-									class="btn btn-secondary buttons-print" tabindex="0" aria-controls="example1"
-									type="button"><span>Print</span></button>
-								<div class="btn-group"><button
-										class="btn btn-secondary buttons-collection dropdown-toggle buttons-colvis"
-										tabindex="0" aria-controls="example1" type="button"
-										aria-haspopup="true"><span>Column visibility</span><span
-											class="dt-down-arrow"></span></button></div>
-							</div>
-						</div>
-						<div class="col-sm-12 col-md-6">
-							<div id="example1_filter" class="dataTables_filter"><label>Search:<input type="search"
-										class="form-control form-control-sm" placeholder=""
-										aria-controls="example1"></label></div>
-						</div>
-					</div>
-					<div class="row">
 						<div class="col-sm-12">
 							<table id="example1" class="table table-bordered table-striped dataTable dtr-inline"
 								aria-describedby="example1_info">
@@ -103,36 +77,7 @@
 							</table>
 						</div>
 					</div>
-					<div class="row">
-						<div class="col-sm-12 col-md-5">
-							<div class="dataTables_info" id="example1_info" role="status" aria-live="polite">Showing 11
-								to 20 of 57 entries</div>
-						</div>
-						<div class="col-sm-12 col-md-7">
-							<div class="dataTables_paginate paging_simple_numbers" id="example1_paginate">
-								<ul class="pagination">
-									<li class="paginate_button page-item previous" id="example1_previous"><a href="#"
-											aria-controls="example1" data-dt-idx="0" tabindex="0"
-											class="page-link">Previous</a></li>
-									<li class="paginate_button page-item "><a href="#" aria-controls="example1"
-											data-dt-idx="1" tabindex="0" class="page-link">1</a></li>
-									<li class="paginate_button page-item active"><a href="#" aria-controls="example1"
-											data-dt-idx="2" tabindex="0" class="page-link">2</a></li>
-									<li class="paginate_button page-item "><a href="#" aria-controls="example1"
-											data-dt-idx="3" tabindex="0" class="page-link">3</a></li>
-									<li class="paginate_button page-item "><a href="#" aria-controls="example1"
-											data-dt-idx="4" tabindex="0" class="page-link">4</a></li>
-									<li class="paginate_button page-item "><a href="#" aria-controls="example1"
-											data-dt-idx="5" tabindex="0" class="page-link">5</a></li>
-									<li class="paginate_button page-item "><a href="#" aria-controls="example1"
-											data-dt-idx="6" tabindex="0" class="page-link">6</a></li>
-									<li class="paginate_button page-item next" id="example1_next"><a href="#"
-											aria-controls="example1" data-dt-idx="7" tabindex="0"
-											class="page-link">Next</a></li>
-								</ul>
-							</div>
-						</div>
-					</div>
+					
 				</div>
 			</div>
 			<!-- /.card-body -->
@@ -281,22 +226,16 @@
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
 <script>
-  $(function () {
-    $("#example1").DataTable({
-      "responsive": true, "lengthChange": false, "autoWidth": false,
-      "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
-    }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
-
-    // $('#example2').DataTable({
-    //   "paging": true,
-    //   "lengthChange": false,
-    //   "searching": false,
-    //   "ordering": true,
-    //   "info": true,
-    //   "autoWidth": false,
-    //   "responsive": true,
-    // });
-  });
+  $('#example1').DataTable({
+    dom: '<"row align-items-center mb-3"' +
+            '<"col-md-7 col-12 mb-2 mb-md-0"B>' +
+            '<"col-md-5 col-12 text-md-right"f>' +
+        '>rtip',
+    buttons: ["copy", "csv", "excel", "pdf", "print", "colvis"],
+    initComplete: function () {
+      $('#example1_wrapper .dataTables_paginate').addClass('pt-3');
+    }
+    });
 </script>
 
 <script>
